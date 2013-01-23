1a8a1a2f982239c131bf8e6d168653828cc3018d
rspeele <peeler.software@gmail.com>
2013-01-22 20:44:27 -0500

    temporarily comment out bin64 check

--- tesseract.bat
+++ tesseract.bat
@@ -2,11 +2,11 @@
 
 set TESS_BIN=bin
 
-IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
-    set TESS_BIN=bin64
-)
-IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
-    set TESS_BIN=bin64
-)
+REM IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
+REM     set TESS_BIN=bin64
+REM )
+REM IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
+REM     set TESS_BIN=bin64
+REM )
 
 start %TESS_BIN%\tesseract.exe "-q$HOME\My Games\Tesseract" -ktesseract -ksauerbraten\packages -glog.txt %*
