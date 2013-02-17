17bdb30234358879762ee5fef960256687e5d998
rspeele <peeler.software@gmail.com>
2013-02-17 00:05:10 -0500

    support bin64 again in bat launcher

--- tersafari.bat
+++ tersafari.bat
@@ -2,11 +2,18 @@
 
 set TSF_BIN=bin
 
-REM IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
-REM     set TSF_BIN=bin64
-REM )
-REM IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
-REM     set TSF_BIN=bin64
-REM )
+IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
+    set TSF_BIN=bin64
+)
+IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
+    set TSF_BIN=bin64
+)
+IF NOT EXIST "%TSF_BIN%" (
+   IF /I "%TSF_BIN%" == "bin64" (
+       set TSF_BIN=bin
+   ) else (
+       set TSF_BIN=bin64
+   )
+)
 
 start %TSF_BIN%\tersafari.exe "-q$HOME\My Games\Tersafari" -ktesseract -ktsfmod -glog.txt %*
